ce6fab46736d144ad8bc4f6fbf21820e4ebcbff5
harmoniques <jerome.bombal@free.fr>
2025-07-25 16:45:10 +0200

    Add files via upload

---
+++ HCJ/datas/sendFormSubscription.php
@@ -0,0 +1,39 @@
+		<?php
+				$mail = strip_tags($_POST['mail']);
+				$checkRobot = strip_tags($_POST['checkRobot']);
+
+				// text to send
+				$texte = "Bonjour,<br /><br />";
+				$texte = $texte . "Message de Hot Coffee Jazz<br />";
+				$texte = $texte . "Vous avez indiqué vouloir souscrire à notre Newsletter :<br />";
+				$texte = $texte . "Email :  $mail<br /><br />";
+				$texte = $texte . "Souscription Newsletter : $newsletter<br /><br />";
+				$texte = $texte . "Message automatique, merci de ne pas y répondre...";
+
+				$texte = stripslashes($texte);
+
+				// Recipient and subject of the message
+				$destinataire = "jerome.bombal@free.fr"; // input your email here
+				$objet = "Message de Hot Coffee Jazz"; // input your domain name here
+
+				// Headers
+	            $headers = array(
+                    'Content-type' => 'text/html',
+                    'From' => 'jerome.bombal@free.fr', // input your email from here
+                    'X-Mailer' => 'PHP/' . phpversion()
+                );
+
+				// Send the message then return data to current page with ajax
+				if ($checkRobot == 7) {
+					$conf = ini_set('mail', 'jerome.bombal@free.fr'); // update your information here
+					$sending_ok = mail($destinataire, $objet, $texte, $headers);
+					if ($sending_ok) {
+							echo "<p class=\"hardLight\">Merci de votre message !<br /><br />Nous y répondrons bientôt</p>";
+						}
+					else {
+							echo "<p class=\"hardLight\">Problême inattendu rencontré...</p>";
+						}
+
+				} else {
+					echo "<p class=\"hardLight\">Problême inattendu rencontré avec le contrôle anti-robot...</p>";
+				}
